ad3dd172d54dc6c2b240c7c478b6df007bd3868a
zhangshine <zhangshine0125@gmail.com>
2020-08-24 22:06:27 +0800

    fix missing country

--- dist/geojson-to-dots.js
+++ dist/geojson-to-dots.js
@@ -12,12 +12,15 @@ var svg_js_1 = require("@svgdotjs/svg.js");
 var fs_1 = __importDefault(require("fs"));
 var jsonData = fs_1.default.readFileSync('../data/ne_10m_admin_0_countries.json', { encoding: 'utf-8' });
 var geojson = JSON.parse(jsonData);
+var allCountryMap = {};
+geojson.features.map(function (value, index) {
+    allCountryMap[value.properties.SOVEREIGNT] = 1;
+});
 var glookup = new geojson_geometries_lookup_1.default(geojson);
 var window = svgdom_1.createSVGWindow();
 var document = window.document;
 svg_js_1.registerWindow(window, document);
-var draw1x1 = svg_js_1.SVG().size(360, 150);
-var result1v1Point = [];
+var result1v1Point = [], countryMap1x1 = {}, missingCountry1x1 = [], missingCountryPoint1x1 = [];
 for (var x = -180; x < 180; x++) {
     for (var y = -90; y < 90; y++) {
         var result = glookup.getContainers({ type: "Point", coordinates: [x, y] });
@@ -25,18 +28,80 @@ for (var x = -180; x < 180; x++) {
             var country = result.features.map(function (value, index) {
                 return value.properties.SOVEREIGNT;
             });
+            countryMap1x1[country[0]] = 1;
             if ('Antarctica' === country[0])
                 continue;
-            draw1x1.circle(0.8).move(x + 180 - .4, -y + 90 - .4);
             result1v1Point.push({ x: x, y: y, country: country });
         }
     }
 }
+Object.keys(allCountryMap).forEach(function (countryName) {
+    if (!countryMap1x1[countryName])
+        missingCountry1x1.push(countryName);
+});
+function polygonAvgPoint(polygonPoints, countryName) {
+    var sumPoint = polygonPoints.reduce(function (accValue, currentValue) {
+        accValue.x += currentValue[0];
+        accValue.y += currentValue[1];
+        return accValue;
+    }, { x: 0, y: 0 });
+    var avgPoint = { x: Math.round(sumPoint.x / polygonPoints.length), y: Math.round(sumPoint.y / polygonPoints.length) };
+    return { x: avgPoint.x, y: avgPoint.y, countryName: countryName };
+}
+function appendIfNotExists(point, target) {
+    for (var i = 0; i < target.length; i++) {
+        var tp = target[i];
+        if (tp.x === point.x && tp.y === point.y && tp.countryName === point.countryName) {
+            return;
+        }
+    }
+    target.push(point);
+}
+geojson.features.forEach(function (countryInfo) {
+    var countryName = countryInfo.properties.SOVEREIGNT;
+    if (missingCountry1x1.indexOf(countryName) != -1) {
+        if (countryInfo.geometry.type === 'Polygon') {
+            var polygonPoints = countryInfo.geometry.coordinates[0];
+            appendIfNotExists(polygonAvgPoint(polygonPoints, countryName), missingCountryPoint1x1);
+        }
+        else if (countryInfo.geometry.type === 'MultiPolygon') {
+            countryInfo.geometry.coordinates.forEach(function (points) {
+                appendIfNotExists(polygonAvgPoint(points[0], countryName), missingCountryPoint1x1);
+            });
+        }
+    }
+});
+// add/merge mission country point
+missingCountryPoint1x1.forEach(function (point) {
+    var found = false;
+    for (var i = 0; i < result1v1Point.length; i++) {
+        var pc = result1v1Point[i];
+        if (pc.x === point.x && pc.y === point.y) {
+            if (pc.country.indexOf(point.countryName) === -1) {
+                pc.country.push(point.countryName);
+                found = true;
+            }
+            else {
+                found = true;
+            }
+        }
+    }
+    if (!found) {
+        result1v1Point.push({
+            x: point.x,
+            y: point.y,
+            country: [point.countryName]
+        });
+    }
+});
+var draw1x1 = svg_js_1.SVG().size(360, 150);
+result1v1Point.forEach(function (point) {
+    draw1x1.circle(0.8).attr({ cx: point.x + 180, cy: -point.y + 90 });
+});
 var svgContent1x1 = draw1x1.svg();
 fs_1.default.writeFileSync('../data/ne_10m_admin_0_countries_1_1x1.svg', svgContent1x1);
 fs_1.default.writeFileSync('../data/ne_10m_admin_0_countries_1_1x1.json', JSON.stringify(result1v1Point, null, 4));
-var draw1x4 = svg_js_1.SVG().size(360, 150);
-var result1v4Point = [];
+var result1v4Point = [], countryMap1x4 = {};
 for (var x = -180; x < 180; x += 2) {
     for (var y = -90; y < 90; y += 2) {
         var result = glookup.getContainers({ type: "Point", coordinates: [x, y] });
@@ -44,13 +109,22 @@ for (var x = -180; x < 180; x += 2) {
             var country = result.features.map(function (value, index) {
                 return value.properties.SOVEREIGNT;
             });
+            countryMap1x4[country[1]] = 1;
             if ('Antarctica' === country[0])
                 continue;
-            draw1x4.circle(1.6).move(x + 180 - .8, -y + 90 - .8);
             result1v4Point.push({ x: x, y: y, country: country });
         }
     }
 }
+//
+// Object.keys(allCountryMap).forEach((countryName)=>{
+//     if(!countryMap1x4[countryName])
+//         console.log(countryName);
+// });
+var draw1x4 = svg_js_1.SVG().size(360, 150);
+result1v4Point.forEach(function (point) {
+    draw1x4.circle(1.6).attr({ cx: point.x + 180, cy: -point.y + 90 });
+});
 var svgContent1x4 = draw1x4.svg();
 fs_1.default.writeFileSync('../data/ne_10m_admin_0_countries_1_2x2.svg', svgContent1x4);
 fs_1.default.writeFileSync('../data/ne_10m_admin_0_countries_1_2x2.json', JSON.stringify(result1v4Point, null, 4));
--- src/geojson-to-dots.ts
+++ src/geojson-to-dots.ts
@@ -11,15 +11,36 @@ const jsonData = fs.readFileSync('../data/ne_10m_admin_0_countries.json', {encod
 
 const geojson = JSON.parse(jsonData);
 
+
+const allCountryMap: {[key:string]: number} = {};
+geojson.features.map((value: any, index: number)=>{
+    allCountryMap[value.properties.SOVEREIGNT] = 1;
+});
+
 const glookup = new GeoJsonGeometriesLookup(geojson);
 
 const window = createSVGWindow();
 const document = window.document;
 registerWindow(window, document);
 
-const draw1x1 = SVG().size(360, 150);
 
-const result1v1Point = [];
+
+interface CountryPoint {
+    x: number;
+    y: number;
+    countryName: string;
+}
+
+interface PointCountries {
+    x: number;
+    y: number;
+    country: string[];
+}
+
+const result1v1Point: PointCountries[] = [],
+    countryMap1x1: {[key:string]: number} = {},
+    missingCountry1x1: string[] = [],
+    missingCountryPoint1x1: CountryPoint[] = [];
 for(let x=-180; x<180; x++) {
     for(let y=-90; y<90; y++) {
         const result = glookup.getContainers({type: "Point", coordinates: [x, y]});
@@ -27,22 +48,89 @@ for(let x=-180; x<180; x++) {
             const country = result.features.map((value: any, index: number)=>{
                 return value.properties.SOVEREIGNT
             });
+            countryMap1x1[country[0]] = 1;
             if('Antarctica' === country[0])
                 continue;
-            draw1x1.circle(0.8).move(x+180-.4, -y+90-.4);
             result1v1Point.push({x, y, country});
         }
     }
 }
 
+Object.keys(allCountryMap).forEach((countryName)=>{
+    if(!countryMap1x1[countryName])
+        missingCountry1x1.push(countryName);
+});
+
+
+function polygonAvgPoint(polygonPoints: any[], countryName: string): CountryPoint {
+    const sumPoint = polygonPoints.reduce((accValue, currentValue)=>{
+        accValue.x += currentValue[0];
+        accValue.y += currentValue[1];
+
+        return accValue;
+    }, {x: 0, y: 0});
+    const avgPoint = {x: Math.round(sumPoint.x / polygonPoints.length), y: Math.round(sumPoint.y / polygonPoints.length)};
+    return {x: avgPoint.x, y: avgPoint.y, countryName};
+}
+
+function appendIfNotExists(point: CountryPoint, target: CountryPoint[]) {
+    for(let i=0; i<target.length; i++) {
+        const tp = target[i];
+        if(tp.x === point.x && tp.y === point.y && tp.countryName === point.countryName){
+            return;
+        }
+    }
+    target.push(point);
+}
+
+geojson.features.forEach((countryInfo: any)=>{
+    const countryName = countryInfo.properties.SOVEREIGNT;
+    if(missingCountry1x1.indexOf(countryName) != -1) {
+        if(countryInfo.geometry.type === 'Polygon') {
+            const polygonPoints: any[] = countryInfo.geometry.coordinates[0];
+            appendIfNotExists(polygonAvgPoint(polygonPoints, countryName), missingCountryPoint1x1)
+        } else if(countryInfo.geometry.type === 'MultiPolygon') {
+            countryInfo.geometry.coordinates.forEach((points: any)=>{
+                appendIfNotExists(polygonAvgPoint(points[0], countryName), missingCountryPoint1x1);
+            });
+        }
+    }
+});
+
+// add/merge mission country point
+missingCountryPoint1x1.forEach((point: CountryPoint)=>{
+    let found = false;
+    for(let i=0; i<result1v1Point.length; i++) {
+        const pc = result1v1Point[i];
+        if(pc.x === point.x && pc.y === point.y) {
+            if(pc.country.indexOf(point.countryName) === -1) {
+                pc.country.push(point.countryName);
+                found = true;
+            } else {
+                found = true;
+            }
+        }
+    }
+
+    if(!found) {
+        result1v1Point.push({
+            x: point.x,
+            y: point.y,
+            country: [point.countryName]
+        });
+    }
+});
+
+const draw1x1 = SVG().size(360, 150);
+result1v1Point.forEach((point)=>{
+    draw1x1.circle(0.8).attr({cx: point.x+180, cy: -point.y+90})
+});
 const svgContent1x1 = draw1x1.svg();
 
 fs.writeFileSync('../data/ne_10m_admin_0_countries_1_1x1.svg', svgContent1x1);
 fs.writeFileSync('../data/ne_10m_admin_0_countries_1_1x1.json', JSON.stringify(result1v1Point, null, 4));
 
-const draw1x4 = SVG().size(360, 150);
-
-const result1v4Point = [];
+const result1v4Point = [], countryMap1x4: {[key:string]: number} = {};
 for(let x=-180; x<180; x+=2) {
     for(let y=-90; y<90; y+=2) {
         const result = glookup.getContainers({type: "Point", coordinates: [x, y]});
@@ -50,14 +138,23 @@ for(let x=-180; x<180; x+=2) {
             const country = result.features.map((value: any, index: number)=>{
                 return value.properties.SOVEREIGNT
             });
+            countryMap1x4[country[1]] = 1;
             if('Antarctica' === country[0])
                 continue;
-            draw1x4.circle(1.6).move(x+180-.8, -y+90-.8);
             result1v4Point.push({x, y, country});
         }
     }
 }
+//
+// Object.keys(allCountryMap).forEach((countryName)=>{
+//     if(!countryMap1x4[countryName])
+//         console.log(countryName);
+// });
 
+const draw1x4 = SVG().size(360, 150);
+result1v4Point.forEach((point)=>{
+    draw1x4.circle(1.6).attr({cx: point.x+180, cy: -point.y+90});
+});
 const svgContent1x4 = draw1x4.svg();
 
 fs.writeFileSync('../data/ne_10m_admin_0_countries_1_2x2.svg', svgContent1x4);
